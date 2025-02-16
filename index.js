dfd3f18a9af84e6269b5bdea7a3f4f320826b7c4
Udolfabi <elo.tiom@gmail.com>
2025-02-16 19:54:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.NTE3Njg2NjQyMDczNjY1NTY2.GGFV94._MI7M_M5nh1JIz7CDwMcesanWmK242-Rsc-gzo
-);
+const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
